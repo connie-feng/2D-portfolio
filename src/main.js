bf2337f54d0a5b3c191daef26904cf2fdc9f1483
Connie Feng <conniefeng28@gmail.com>
2024-04-21 19:23:22 -0500

    Add player

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { k } from "./kaboom.Ctx";
+import { scaleFactor } from "./constants";
+import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
@@ -12,3 +13,33 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
   },
 });
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#d4c8f7"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 3), 10, 10)
+    }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    },
+    "player",
+  ]);
+});
+
+k.go("main");
